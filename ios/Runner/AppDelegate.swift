8af8488c13b4a4681cfe7f551230918e1dbbd1a3
TAGDEV TECHNOLOGIES LIMITED <info@tagdev.tech>
2021-05-29 21:19:27 +0100

    ios setup

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,6 +1,7 @@
 import UIKit
 import Flutter
 import flutter_background_service // add this
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -12,7 +13,14 @@ import flutter_background_service // add this
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyCRrfbYY6rhh2qfMPo-My7y_gUfsl2eP14")
     GeneratedPluginRegistrant.register(with: self)
+    if #available(iOS 10.0, *) {
+      UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
+    }
+    SwiftFlutterBackgroundServicePlugin.setPluginRegistrantCallback { registry in
+                AppDelegate.registerPlugins(with: registry)
+            }
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
